ea8757bd28f8e71c34042c1798798e60d8a85122
Jamshid <elmurodovjamshid38@gmail.com>
2025-04-29 20:45:18 +0500

    feat: read configs from config toml file

--- server/src/main/java/uz/server/ServerApplication.java
+++ server/src/main/java/uz/server/ServerApplication.java
@@ -1,11 +1,37 @@
 package uz.server;
 
+import com.moandjiezana.toml.Toml;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        String path = Arrays.stream(args).filter(s -> s.startsWith("--config=")).findFirst().orElseThrow(
+                () -> new RuntimeException("Config path not provided")
+        );
+
+        Map<String, Object> props = loadToml(path);
+
+        SpringApplication app = new SpringApplication(ServerApplication.class);
+        app.setDefaultProperties(props);
+        app.run(args);
+    }
+
+    public static Map<String, Object> loadToml(String path){
+        Toml toml = new Toml().read(new File(path));
+        Map<String, Object> map = new HashMap<>();
+        map.put("spring.datasource.url", toml.getString("spring.datasource.url"));
+        map.put("spring.datasource.username", toml.getString("spring.datasource.username"));
+        map.put("spring.datasource.password", toml.getString("spring.datasource.password"));
+        map.put("github.client-id", toml.getString("github.client-id"));
+        map.put("github.client-secret", toml.getString("github.client-secret"));
+        map.put("github.redirect-uri", toml.getString("github.redirect-uri"));
+        return map;
     }
 }
