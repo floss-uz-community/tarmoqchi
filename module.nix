ca962fcd44545cae7f2836be4ef7c508f2f98996
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-04-30 13:07:31 +0500

    fix: back to app reference on port

--- module.nix
+++ module.nix
@@ -22,7 +22,7 @@ flake: {
 
   # The digesting configuration of server
   toml-config = toml.generate "config.toml" {
-    server.port = toString cfg.port;
+    app.port = toString cfg.port;
     spring.datasource.url = "#databaseUrl#";
     github = {
       client-id = "#ghcid#";
