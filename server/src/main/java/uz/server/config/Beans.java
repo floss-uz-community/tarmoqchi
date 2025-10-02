1e48cd23e64bdefd62691aa1605dfc562bc7e779
Jamshid <elmurodovjamshid38@gmail.com>
2025-10-02 15:13:15 +0500

    feat: cors error fixed

--- server/src/main/java/uz/server/config/Beans.java
+++ server/src/main/java/uz/server/config/Beans.java
@@ -3,6 +3,8 @@ package uz.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Beans {
@@ -10,4 +12,18 @@ public class Beans {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+      }
+    };
+  }
 }
